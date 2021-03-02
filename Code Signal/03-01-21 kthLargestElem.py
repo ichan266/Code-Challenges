b260ee9439adefe6b259af34c9e6d94f94aec991
Iris Kuhn <ichan266@hotmail.com>
2021-03-01 17:46:04 -0800

    3/1/21 Code Signal (S+Q): kthLargestElem

---
+++ Code Signal/03-01-21 kthLargestElem.py
@@ -0,0 +1,24 @@
+def kthLargestElement(nums, k):
+    
+    newList = [nums[0]]
+    tempList = []
+        
+    for idx in range(1,len(nums)):
+        firstNum = newList[0]
+        lastNum = newList[-1]
+        if nums[idx] >= lastNum:
+            newList.append(nums[idx])
+        elif nums[idx] <= firstNum:
+            newList.insert(0, nums[idx])
+        else:
+            while nums[idx] < newList[-1]:
+                tempList.append(newList.pop())
+            newList.append(nums[idx])
+            tempList.reverse()
+            newList.extend(tempList)
+            tempList = []
+            
+    return newList[len(nums)-k]
+
+print(kthLargestElement([3, 2, 1, 5, 6, 4], 2))
+ls = [3, 2, 1, 5, 6, 4]
