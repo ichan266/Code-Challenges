451caf7a54a2f7e37624f52fbe4f89107bb04abf
Iris Kuhn <ichan266@hotmail.com>
2021-04-14 20:57:01 -0700

    Adding more test cases for Nth Tribonacci

--- Leetcode/04-14-21 NthTribonacci.py
+++ Leetcode/04-14-21 NthTribonacci.py
@@ -3,6 +3,7 @@
 # T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
 # Given n, return the value of Tn.
 
+import timeit
 class Solution:
 
     cache = {}
@@ -16,7 +17,7 @@ class Solution:
             return 1
         else: 
             if n not in self.cache:
-                self.cache[n] = self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1)
+                self.cache[n] = self.tribonacci1(n-3) + self.tribonacci1(n-2) + self.tribonacci1(n-1)
         
         return self.cache[n]
 
@@ -30,8 +31,43 @@ class Solution:
 
         return self.tribonacci2(n-3) + self.tribonacci2(n-2) + self.tribonacci2(n-1)
 
+    # % Master Ben's solution
+    def tribonacci3(self, n: int) -> int:
+        
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        
+        array = [0, 1, 1]
+        
+        for idx in range(3, n+1):
+            array.append(array[idx-3] + array[idx-2] + array[idx-1])
+            
+        return array[n]
+
+    def tribonacci4(self, n: int) -> int:
+        
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        
+        array = [0] * (n+1)
+        array[1], array[2] = 1, 1
+        
+        for idx in range(3, n+1):
+            array[idx] = array[idx-3] + array[idx-2] + array[idx-1]
+            
+        return array[n]
 
 instance = Solution()
 
-print(instance.tribonacci2(4))
-print(instance.tribonacci2(25))
+#print(timeit.timeit(lambda:instance.tribonacci2(20), number=10))
+print(timeit.timeit(lambda:instance.tribonacci1(2000), number=10))
+print(timeit.timeit(lambda:instance.tribonacci3(2000), number=10))
+print(timeit.timeit(lambda:instance.tribonacci4(2000), number=10))
+
+# print(instance.tribonacci3(400))
+# print(instance.tribonacci4(400))
+# print(instance.tribonacci2(25))
