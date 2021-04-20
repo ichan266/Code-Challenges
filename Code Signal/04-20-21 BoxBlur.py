969029fed04b232b7e84d35caf0d821f0984c5dd
Iris Kuhn <ichan266@hotmail.com>
2021-04-20 14:55:13 -0700

    4/20/21 CodeSignal (BoxBlur)

---
+++ Code Signal/04-20-21 BoxBlur.py
@@ -0,0 +1,14 @@
+# Code Signal Arcade 
+def boxBlur(image):
+
+    croppedImageWdth = len(image[0])-2
+    croppedImageHt = len(image)-2
+    
+    croppedImage = [[0 for y in range(croppedImageWdth)] for x in range(croppedImageHt)]
+    
+    for indexRow in range(croppedImageHt):
+        for indexColumn in range(croppedImageWdth):
+            total = sum(image[indexRow][indexColumn:indexColumn+3]) + sum(image[indexRow+1][indexColumn:indexColumn+3]) + sum(image[indexRow+2][indexColumn:indexColumn+3])
+            croppedImage[indexRow][indexColumn] = total//9
+            
+    return croppedImage
